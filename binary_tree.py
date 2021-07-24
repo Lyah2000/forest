3a42e0630060fab17f7a215b2b4fe42da1441e00
Lyah2000 <elizaveta.lyahovskaya@gmail.com>
2021-07-24 17:17:28 +0300

    create base Node

--- binary_tree.py
+++ binary_tree.py
@@ -4,7 +4,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
+class Node:
+    def __init__(self, base):
+        self.left = None
+        self.right = None
+        self.base = base
+
+    def print_(self):
+        print(self.base)
+
 
 if __name__ == '__main__':
     print('Hello world')
-    
+    one = Node(15)
+    one.print_()
